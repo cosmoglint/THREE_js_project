a72584e35d140faee4536595e27896856c1a92db
thedoodler <ramprakashsmail@gmail.com>
2020-09-12 09:24:43 +0530

    colors

--- sketch.js
+++ sketch.js
@@ -17,19 +17,30 @@ window.addEventListener('resize', () => {renderer.setSize(window.innerWidth,wind
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 
+function random_color(){
+	var r = Math. floor(Math. random() * 256);
+	var g = Math. floor(Math. random() * 256);
+	var b = Math. floor(Math. random() * 256);
+	return ("rgb("+r+","+g+","+b+")");
+	
+}
+
+
 
 var sph1 = new THREE.SphereGeometry(1,20,20);
 var box1  = new THREE.BoxGeometry(1,1,1 );
 var pink_material = new THREE.MeshLambertMaterial({color: 0xFF00CC});
 var yellow_material = new THREE.MeshLambertMaterial({color: 0xFFCC11});
 var teal_material = new THREE.MeshLambertMaterial({color: 0x7744FF});
+var red_material = new THREE.MeshLambertMaterial({color:"rgb(255,140,140)"});
+var changing_material = THREE.MeshLambertMaterial({color: random_color()});
 var sph_mesh = new THREE.Mesh(sph1,teal_material);
-var box_mesh = new THREE.Mesh(box1,yellow_material)
+var box_mesh = new THREE.Mesh(box1,red_material);
 
 scene.add(box_mesh);
 scene.add(sph_mesh);
 
-var light = new THREE.PointLight(0xFFFFFF,1,500);
+var light = new THREE.PointLight("rgb(255,140,140)",1,500);
 sph_mesh.position.set(3,0,-1);
 box_mesh.position.set(-1,1,-3);
 light.position.set(10,0,25);
@@ -54,7 +65,8 @@ function on_moved(event){
 	for (var i=0; i<intersects.length; i++){
 		this.tl = new TimelineMax();
 		this.tl.to(intersects[i].object.scale, 1 ,{x:2,ease:Expo.easeout});
-		this.tl.to(intersects[i].object.scale, 1 ,{x:0,ease:Expo.easeout});
+		this.tl.to(intersects[i].object.scale, 1 ,{x:-1,ease:Expo.easeout});
+		var changing_material = THREE.MeshLambertMaterial({color: random_color()});
 	}
 	
 }
