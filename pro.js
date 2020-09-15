b228b98b0b35512d54167dd692e496e653633bce
thedoodler <ramprakashsmail@gmail.com>
2020-09-15 11:07:23 +0530

    working skeleton framework

--- pro.js
+++ pro.js
@@ -10,10 +10,38 @@ let renderer;
 
 
 function init(){
-	scene = new THREE.scene();
+	scene = new THREE.Scene();
 	
 	const fov = 75;
 	const aspect_ratio = w_width/w_height;
 	const near = 0.1;
 	const far = 1000;
+	
+	camera = new THREE.PerspectiveCamera(fov,aspect_ratio,near,far);
+	camera.position.z = 0;
+	
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize(w_width,w_height);
+	renderer.setClearColor("#f4f4f4");
+	
+	document.body.appendChild(renderer.domElement);
+	
 }
+
+window.addEventListener('resize', () => {
+	new_sizes();
+	renderer.setSize(w_width,w_height);
+	camera.aspect = (w_width/w_height);
+	camera.updateProjectionMatrix;
+})
+
+function render(){
+	renderer.render(scene,camera);
+}
+
+function main_loop(){
+	init();
+	render();
+}
+
+main_loop();
